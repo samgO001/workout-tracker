0e8d1bd0c083f0bf7726fefaf434bd3b3ccc1365
Samuel Guarin <guarin090vvvgmail.com>
2025-10-03 15:48:43 -0500

    feat(users): implement GET endpoints for all users and by id

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require("express"); // Import express
+const { port } = require("./config/env"); // Importar la configuración
 const app = express(); // Create an instance of express
-const port = 8000; // puerto de escucha
 
 // Inicializacion del servidor y primera ruta
 app.get("/", (req, res) => {
   res.send("Hola mi server en Express");
 });
 
-// Inicio del servidor
+// Iniciar el servidor usando el puerto de la configuración
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
